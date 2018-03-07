d2110e42ffeb52185a2a2828fa9e944a262996c3
Christophe Massolin <christophe.massolin@makemepulse.com>
2018-03-07 17:13:59 +0100

    Add npm and wk shortcuts

--- bin/npm-do.js
+++ bin/npm-do.js
@@ -1,11 +1,35 @@
 #!/usr/bin/env node
 
+const { statSync }  = require( 'fs' )
 const { spawnSync } = require( 'child_process' )
 
 const NPM_BIN_PATH = spawnSync('npm', [ 'bin' ], { shell: true, encoding: 'utf-8' }).stdout.trim()
 
 const ARGV = process.argv.slice(2)
 
-const CMD = NPM_BIN_PATH + '/' + ARGV.shift()
+function path_exists(path) {
+  try {
+    statSync( path )
+  } catch(e) {
+    return false
+  }
 
-spawnSync( CMD, ARGV, { shell: true, stdio: 'inherit' } )
+  return true
+}
+
+const CLIS = [ 'npm', 'wk', '__local__' ]
+
+for (let i = 0, ilen = CLIS.length, cli = null; i < ilen; i++) {
+  cli = CLIS[i]
+
+  if (cli === 'wk') {
+    cli = NPM_BIN_PATH + '/wk'
+  } else if (cli === 'npm') {
+    ARGV.unshift( 'run' )
+  } else if (cli === '__local__') {
+    cli = NPM_BIN_PATH + '/' + ARGV.shift()
+  }
+
+  spawnSync( cli, ARGV, { shell: true, stdio: 'inherit' } )
+  break;
+}
